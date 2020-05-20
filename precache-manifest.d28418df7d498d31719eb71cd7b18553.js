1ae410360994da2a6f3d28366ff99b97078c05a0
Ajai John Chemmanam <ajaichemmanam@gmail.com>
2020-05-21 00:29:20 +0530

    Updates

---
+++ precache-manifest.d28418df7d498d31719eb71cd7b18553.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cbf9f63119896fbf289aee824bf0aa3",
+    "url": "/react-3D-keypad/index.html"
+  },
+  {
+    "revision": "7b3b179844cefd5573a3",
+    "url": "/react-3D-keypad/static/css/main.f6c7fb9b.chunk.css"
+  },
+  {
+    "revision": "d8edb7a5be549f4418d8",
+    "url": "/react-3D-keypad/static/js/2.f967f03d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-3D-keypad/static/js/2.f967f03d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b3b179844cefd5573a3",
+    "url": "/react-3D-keypad/static/js/main.202b6bb7.chunk.js"
+  },
+  {
+    "revision": "059a7e3cce52a5fe34ee",
+    "url": "/react-3D-keypad/static/js/runtime-main.8b111e42.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-3D-keypad/static/media/logo.5d5d9eef.svg"
+  }
+]);
